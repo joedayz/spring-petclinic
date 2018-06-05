3362e4211d730c348c66be14f28713a46987c34b
Jose Diaz <jose.diaz@joedayz.pe>
2018-06-04 21:31:04 -0500

    convertir jar a war en gradle

--- src/main/java/com/tumi/demo/DemoApplication.java
+++ src/main/java/com/tumi/demo/DemoApplication.java
@@ -2,11 +2,19 @@ package com.tumi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }
