b6b7e0613d7aefcc3e5b61db9be998bf4312d0da
Jose Diaz <jose.diaz@joedayz.pe>
2018-06-06 19:35:57 -0500

    version final

--- src/main/java/com/tumi/demo/DemoApplication.java
+++ src/main/java/com/tumi/demo/DemoApplication.java
@@ -2,16 +2,16 @@ package com.tumi.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
+//@ComponentScan("com.tumi.demo")
+public class DemoApplication {//extends SpringBootServletInitializer {
 
-	@Override
+/*	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(DemoApplication.class);
-	}
+	}*/
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DemoApplication.class, args);
